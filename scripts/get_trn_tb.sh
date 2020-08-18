6a746bd02ae311bdec025bc4f4e619e863fbf799
Manu Flores <emanuelflrsb@hotmail.com>
2020-08-18 15:16:46 -0700

    Added mini py file for plotting style, new notebook with SDR logFC values, and shell scripts to download data from colombos and the TRN.

---
+++ scripts/get_trn_tb.sh
@@ -0,0 +1,2 @@
+wget -O mtube_trn.txt https://doi.org/10.1371/journal.pone.0022178.s003
+
---
+++ tbredox/get_colombos_data_tb.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget -O tb_colombos.zip http://colombos.net/cws_data/compendium_data/mtube_compendium_data.zip
+#Add condition to unzip file
+#unzip tb_colombos.zip
